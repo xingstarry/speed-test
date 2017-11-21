52e083bc47bf867e862f45e613f7d568ef2fb4f1
xingstarry <1816866520@qq.com>
2017-11-21 15:27:40 +0800

    20171121

---
+++ js/index.js
@@ -0,0 +1,40 @@
+(function(win,doc){
+    function change(){
+        doc.documentElement.style.fontSize = 20 * document.documentElement.clientWidth / 375 + 'px';
+    };
+    win.addEventListener('resize',change,false);
+    change();
+})(window,document);
+
+Zepto(function($){
+    var oSecBg = $('.secBg');
+    var oHeader = $('header');
+    var oNav = $('nav');
+
+    var aNavL = $('nav li');
+    var aNavA = $('nav li a');
+    var oSection = $('section');
+    var aDiv = oSection.children();
+
+    var aVList = $('.vList li a');
+
+    //*********************背景*************************
+    var secH = $(window).height() - oHeader.height() - oNav.height();
+    oSecBg.css('height',secH+'px');
+
+    //*********************导航切换*************************
+    aNavL.on('click',function () {
+        aDiv.hide();
+        aDiv.eq($(this).index()).css('display','block');
+        aNavL.removeClass('active');
+        aNavA.css('color','#fff');
+        $(this).addClass('active');
+        aNavA.eq($(this).index()).css('color','#62E809');
+    });
+
+    //***********************测试点*************************
+    aVList.on('click',function () {
+        aVList.removeClass('active');
+        $(this).addClass('active');
+    })
+});
